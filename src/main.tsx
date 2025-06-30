fd74f4014d246f04ebc9377016ed2fdad5aae771
dayeon <aakk9350@naver.com>
2025-06-30 19:32:40 +0900

    feat: global style and reset css

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
 import App from '@/App';
+import GlobalStyle from '@/styles/GlobalStyle';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </StrictMode>,
