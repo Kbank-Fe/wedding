f102a91a32858d44851034577a922c06eddcd75f
dayeon <aakk9350@naver.com>
2025-06-30 18:27:54 +0900

    setting: file-based routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import Pages from 'vite-plugin-pages';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Pages({
+      dirs: 'src/pages',
+      extensions: ['tsx'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
