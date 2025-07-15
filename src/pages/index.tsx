051c6306b8c11384706c4273159786c24294fccb
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2025-07-15 15:34:55 +0900

    Style/13 modify pagelayout component (#14)
    * fix: PageLayout 개선
    
    * feat: 페이지 스크롤을 위한 Section 컴포넌트 추가
    
    * feat: 개별 컴포넌트 Section으로 감싸기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
 import AccountList from '@/components/AccountList';
 import DateCalendar from '@/components/calendar/DateCalendar';
-import PageLayout from '@/components/PageLayout';
-import WeddingIntro from '@/components/WeddingIntro';
+import PageLayout from '@/components/shared/PageLayout';
+import Section from '@/components/shared/Section';
 import TransferList from '@/components/transfer/TransferList';
+import WeddingIntro from '@/components/WeddingIntro';
 
 const HomePage = () => {
   return (
     <PageLayout>
-      <WeddingIntro />
-      <DateCalendar
-        day={15}
-        hour={21}
-        min={30}
-        month={7}
-        msec={0}
-        sec={0}
-        year={2026}
-      />
-      <TransferList />
-      <AccountList />
+      <Section>
+        <WeddingIntro />
+      </Section>
+      <Section>
+        <DateCalendar
+          day={15}
+          hour={21}
+          min={30}
+          month={7}
+          msec={0}
+          sec={0}
+          year={2026}
+        />
+      </Section>
+      <Section>
+        <AccountList />
+      </Section>
+      <Section>
+        <TransferList />
+      </Section>
     </PageLayout>
   );
 };
