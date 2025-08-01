e7f7ff3d39b91d8c65b0d8ab9d1f967acb88a2a9
Park JinHyeong <pjhyng0125@naver.com>
2025-08-01 15:53:48 +0900

    feat: Contact 컴포넌트 개발 (#30)
    feat: Contact 컴포넌트 개발 (#30)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import AccountList from '@/components/account/AccountList';
 import DateCalendar from '@/components/calendar/DateCalendar';
+import Contact from '@/components/contact/Contact';
+import WeddingIntro from '@/components/Intro/WeddingIntro';
 import PageLayout from '@/components/shared/PageLayout';
 import Section from '@/components/shared/Section';
 import TransferList from '@/components/transfer/TransferList';
-import WeddingIntro from '@/components/Intro/WeddingIntro';
 
 const HomePage = () => {
   return (
     <PageLayout>
       <Section>
         <WeddingIntro />
+      </Section>
+      <Section>
+        <Contact />
       </Section>
       <Section>
         <DateCalendar
