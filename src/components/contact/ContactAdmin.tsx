c3613c2313a463e0cf0a1c417e41246ee50a0865
JinHyeong Park <pjhyng0125@naver.com>
2025-08-11 22:51:59 +0900

    feat: input text number keypad 컴포넌트 기능 구현

---
+++ src/components/contact/ContactAdmin.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import BaseNumberKeypadTextInput from '@/components/shared/BaseNumberKeypadTextInput';
+
+const ContactAdmin = () => {
+  const [phone, setPhone] = useState<string>('');
+
+  useEffect(() => {
+    console.log('Selected phone:', phone);
+  }, [phone]);
+
+  const handleEmit = (text: string) => {
+    setPhone(text);
+  };
+  return (
+    <>
+      <BaseNumberKeypadTextInput
+        className=""
+        placeholder="번호를 입력해주세요"
+        onEmit={handleEmit}
+      />
+    </>
+  );
+};
+
+export default ContactAdmin;
