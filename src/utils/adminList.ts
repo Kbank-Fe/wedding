9289cb809d616d900e9918e26b78edbe8ba47757
devKyu <barley12@naver.com>
2025-08-22 12:37:44 +0900

    feat: adminList 추가

--- src/utils/adminList.ts
+++ src/utils/adminList.ts
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 
+import BaseTextArea from '@/components/shared/BaseTextArea';
 import TransportAdmin from '@/components/transport/TransportAdmin';
 
 type AdminList = {
@@ -13,5 +14,10 @@ export const adminList: AdminList[] = [
     title: '교통수단',
     value: 'transport',
     component: TransportAdmin,
+  },
+  {
+    title: 'textArea',
+    value: 'textArea',
+    component: BaseTextArea,
   },
 ];
