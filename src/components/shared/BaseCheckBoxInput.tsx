42acbe879aba3550a846908d719208724f64027c
arionk0905 <kimhy0905@gmail.com>
2025-08-29 16:06:09 +0900

    fix: 체크박스 컴포넌트 수정 (#53)
    * fix: 체크박스 컴포넌트 수정
    
    * fix:예제 추가
    
    * fix: props 전달 시 rest로 하도록 수정
    
    * fix:이름 변경
    
    * fix: id사용하는 부분 제거
    
    * fix: 색상부분 radix color 사용하도록 수정
    
    * fix: 명칭 변경
    
    * fix: 명칭 수정
    
    * fix: 파일명칭 변경

---
+++ src/components/shared/BaseCheckBoxInput.tsx
@@ -0,0 +1,62 @@
+import { css } from '@emotion/react';
+
+type BaseCheckBoxInputProps = {
+  checkboxLabel: string;
+  checked: boolean;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const BaseCheckBoxInput = ({
+  checkboxLabel,
+  checked,
+  ...rest
+}: BaseCheckBoxInputProps) => {
+  return (
+    <div>
+      <label css={labelStyle}>
+        <input
+          checked={checked}
+          css={checkboxStyle}
+          type="checkbox"
+          {...rest}
+        />
+        <span>{checkboxLabel}</span>
+      </label>
+    </div>
+  );
+};
+
+const checkboxStyle = css`
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  width: 20px;
+  height: 20px;
+  border-radius: 4px;
+  background-color: var(--gray5);
+  position: relative;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+
+  &:checked {
+    background-color: var(--gray12);
+  }
+
+  &:checked::after {
+    content: '✔';
+    font-size: 14px;
+    color: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const labelStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 8px; /* 체크박스와 텍스트 사이 간격 */
+  cursor: pointer;
+`;
+export default BaseCheckBoxInput;
