34dd699ba99aacb1afcb8a922298093aaaba38e2
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2025-09-16 17:11:15 +0900

    Refactor/37 create loading spinner (#88)
    * feat: create LoadingSpinner component
    
    * refactor: apply LoadingSpinner

---
+++ src/components/shared/LoadingSpinner.tsx
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react';
+
+const LoadingSpinner = () => {
+  return (
+    <div css={containerStyle}>
+      <div css={spinnerStyle} />
+    </div>
+  );
+};
+
+const containerStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.75rem;
+  min-height: 100vh;
+`;
+
+const spinnerStyle = css`
+  height: 2.4rem;
+  width: 2.4rem;
+  border-radius: 50%;
+  border: 6px solid var(--gray3);
+  border-top-color: var(--gray5);
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default LoadingSpinner;
