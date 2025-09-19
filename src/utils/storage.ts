5baabb1a6328358aab84451740f0da3ef37567d1
dayeon <aakk9350@naver.com>
2025-09-19 17:02:10 +0900

    feat: uid 가공

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,11 +3,18 @@ import { getDownloadURL, ref as sRef, uploadBytes } from 'firebase/storage';
 import { storage } from '@/utils/firebase';
 import { compressImage } from '@/utils/image.ts';
 
+const PREFIX = 'kakao:';
+
 export const uploadImageToStorage = async (file: File, uid: string) => {
   const blob = await compressImage(file, { maxWidth: 1920, quality: 0.8 });
 
   const fileName = `${Date.now()}_${crypto.randomUUID()}.webp`;
-  const path = `gallery/${uid}/${fileName}`;
+
+  let id = '';
+  if (uid.includes(PREFIX)) {
+    id = uid.replaceAll(PREFIX, '');
+  }
+  const path = `gallery/${id}/${fileName}`;
   const storageRef = sRef(storage, path);
 
   await uploadBytes(storageRef, blob, { contentType: blob.type });
