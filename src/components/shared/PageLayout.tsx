c02aaccf3a80b066e7ea26063cabedd75e97bb3b
dazzel3 <aakk9350@naver.com>
2025-09-30 23:12:29 +0900

    feat: pageType 추가

--- src/components/shared/PageLayout.tsx
+++ src/components/shared/PageLayout.tsx
@@ -1,31 +1,37 @@
 import { css } from '@emotion/react';
 import type { ReactNode } from 'react';
 
+type PageType = 'basic' | 'main' | 'login';
+
 type PageLayoutProps = {
   children: ReactNode;
-  main?: boolean;
+  pageType?: PageType;
 };
 
-const PageLayout = ({ children, main = false }: PageLayoutProps) => {
+const PageLayout = ({ children, pageType = 'basic' }: PageLayoutProps) => {
   return (
-    <div css={layoutStyle({ main })}>
-      <div css={wrapperStyle}>{children}</div>
+    <div css={layoutStyle({ pageType })}>
+      <div css={wrapperStyle({ pageType })}>{children}</div>
     </div>
   );
 };
 
-const layoutStyle = ({ main }: { main: boolean }) => css`
+const layoutStyle = ({ pageType }: { pageType: PageType }) => css`
   width: 100%;
   min-height: 100vh;
   max-width: 500px;
   margin: 0 auto;
-  background: var(--gray1);
-  font-family: ${main ? "'Wedding', sans-serif" : "inherit"};
+  background: ${pageType === 'main' || pageType === 'login'
+    ? '#F8F6F1'
+    : 'var(--gray1)'};
+  font-family: ${pageType === 'main' || pageType === 'login'
+    ? "'Wedding', sans-serif"
+    : 'inherit'};
 `;
 
-const wrapperStyle = css`
+const wrapperStyle = ({ pageType }: { pageType: PageType }) => css`
   width: 100%;
-  padding: 3rem 0;
+  padding: ${pageType === 'login' ? 0 : '3rem 0'};
 `;
 
 export default PageLayout;
