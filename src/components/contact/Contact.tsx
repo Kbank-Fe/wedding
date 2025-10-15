96abe00756d66d38c1637a55ef7fd03e88bc1a79
JinHyeong Park <pjhyng0125@naver.com>
2025-10-16 00:13:36 +0900

    feat: 컨텐츠 모달 컴포넌트 모듈화 및 Contact 적용

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,109 +1,16 @@
-import { css } from '@emotion/react';
-import * as Dialog from '@radix-ui/react-dialog';
-
-import ContactModal from './ContactModal';
-
-// HEX → RGBA 변환 함수
-const hexToRgba = (hex: string, alpha: number) => {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
-  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-};
+import ContactModal from '@/components/contact/ContactModal';
+import ContentModal from '@/components/shared/ContentModal';
 
 const Contact = () => {
   return (
-    <Dialog.Root>
-      {/* 모달 열기 버튼 */}
-      <Dialog.Trigger asChild>
-        <button css={modalButtonStyle}>연락하기</button>
-      </Dialog.Trigger>
-
-      <Dialog.Portal>
-        {/* 검정 반투명 배경 */}
-        <Dialog.Overlay css={modalOverlayStyle} />
-
-        {/* 전체 컨텐츠 */}
-        <Dialog.Content css={modalContentStyle}>
-          {/* 상단 헤더 */}
-          <header css={modalHeaderStyle}>
-            <Dialog.Title css={modalTitleStyle}>연락 하기</Dialog.Title>
-
-            <Dialog.Description css={modalDescriptionStyle}>
-              연락하기 모달창입니다
-            </Dialog.Description>
-
-            {/* 닫기 버튼 (우측 상단) */}
-            <Dialog.Close asChild>
-              <button css={modalCloseStyle}>✕</button>
-            </Dialog.Close>
-          </header>
-
-          {/* 본문 */}
-          <main css={modalMainStyle}>
-            <ContactModal />
-          </main>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <ContentModal
+      buttonText="연락 하기"
+      description="연락하기 모달창입니다"
+      title="연락 하기"
+    >
+      <ContactModal />
+    </ContentModal>
   );
 };
-
-const modalButtonStyle = css`
-  margin-bottom: 1rem;
-  padding: 0.5rem 1rem;
-  background-color: var(--gray4);
-  color: var(--gray12);
-  border-radius: 1rem;
-  font-size: 1rem;
-`;
-
-const modalOverlayStyle = css`
-  position: fixed;
-  inset: 0;
-  /* background-color: var(--gray12); */
-  /* background-color: rgba(0, 0, 0, 0.8); */
-  background-color: ${hexToRgba('#111111', 0.8)};
-`;
-
-const modalContentStyle = css`
-  position: fixed;
-  inset: 0;
-  display: flex;
-  flex-direction: column;
-  color: white;
-  background: transparent;
-`;
-
-const modalHeaderStyle = css`
-  position: relative;
-  padding: 1.5rem;
-  text-align: center;
-`;
-
-const modalTitleStyle = css`
-  font-size: 1rem;
-  font-weight: bold;
-`;
-
-const modalDescriptionStyle = css`
-  display: none;
-`;
-
-const modalCloseStyle = css`
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
-  background: transparent;
-  border: none;
-  font-size: 1.5rem;
-  color: var(--gray1);
-  cursor: pointer;
-`;
-
-const modalMainStyle = css`
-  flex: 1;
-  padding: 2rem;
-`;
 
 export default Contact;
---
+++ src/components/shared/ContentModal.tsx
@@ -0,0 +1,111 @@
+import { css } from '@emotion/react';
+import * as Dialog from '@radix-ui/react-dialog';
+
+type ContentModalProps = {
+  children: React.ReactNode;
+  buttonText: string;
+  title: string;
+  description?: string;
+  onClose?: () => void;
+};
+
+const ContentModal = ({
+  children,
+  buttonText,
+  title,
+  description,
+  onClose,
+}: ContentModalProps) => {
+  return (
+    <Dialog.Root>
+      {/* 모달 열기 버튼 */}
+      <Dialog.Trigger asChild>
+        <button css={modalButtonStyle}>{buttonText}</button>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        {/* 검정 반투명 배경 */}
+        <Dialog.Overlay css={modalOverlayStyle} />
+
+        {/* 전체 컨텐츠 */}
+        <Dialog.Content css={modalContentStyle}>
+          {/* 상단 헤더 */}
+          <header css={modalHeaderStyle}>
+            <Dialog.Title css={modalTitleStyle}>{title}</Dialog.Title>
+
+            <Dialog.Description css={modalDescriptionStyle}>
+              {description}
+            </Dialog.Description>
+
+            {/* 닫기 버튼 (우측 상단) */}
+            <Dialog.Close asChild>
+              <button css={modalCloseStyle} onClick={onClose}>
+                ✕
+              </button>
+            </Dialog.Close>
+          </header>
+
+          {/* 본문 */}
+          <main css={modalMainStyle}>{children}</main>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
+
+const modalButtonStyle = css`
+  margin-bottom: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: var(--gray4);
+  color: var(--gray12);
+  border-radius: 1rem;
+  font-size: 1rem;
+`;
+
+const modalOverlayStyle = css`
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+
+const modalContentStyle = css`
+  position: fixed;
+  inset: 0;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  background: transparent;
+`;
+
+const modalHeaderStyle = css`
+  position: relative;
+  padding: 1.5rem;
+  text-align: center;
+`;
+
+const modalTitleStyle = css`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+const modalDescriptionStyle = css`
+  display: none;
+`;
+
+const modalCloseStyle = css`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  background: transparent;
+  border: none;
+  font-size: 1.5rem;
+  color: var(--gray1);
+  cursor: pointer;
+`;
+
+const modalMainStyle = css`
+  flex: 1;
+  padding: 2rem;
+`;
+
+export default ContentModal;
