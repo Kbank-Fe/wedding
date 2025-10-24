733389eb92eb188aad0142b2930f26ef3abd428b
devKyu <barley12@naver.com>
2025-10-24 15:40:43 +0900

    style: 스타일 개선

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,10 +3,10 @@ import { css } from '@emotion/react';
 const Footer = () => {
   return (
     <div css={containerStyle}>
-      <div css={buttonContainerStyle}>
+      {/* <div css={buttonContainerStyle}>
         <button css={buttonStyle}>카카오톡 공유</button>
         <button css={buttonStyle}>링크 공유</button>
-      </div>
+      </div> */}
       <img alt="Us Day Logo" css={imageStyle} src="/images/logo.png" />
 
       <p css={fontStyle}>© usday. All rights reserved.</p>
@@ -17,34 +17,35 @@ const Footer = () => {
 const containerStyle = css`
   background-color: var(--gray2);
   width: 100%;
-  height: 25vh;
+  height: 23vh;
 
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 0.3rem;
+  gap: 0.8rem;
 `;
 
-const buttonContainerStyle = css`
-  display: flex;
-  gap: 0.5rem;
-  margin-bottom: 1rem;
-`;
+// const buttonContainerStyle = css`
+//   display: flex;
+//   gap: 0.5rem;
+//   margin-bottom: 1rem;
+// `;
 
-const buttonStyle = css`
-  border: 1px solid var(--gray4);
-  padding: 0.5rem 3rem;
-  border-radius: 0.5rem;
-  color: var(--gray11);
-`;
+// const buttonStyle = css`
+//   border: 1px solid var(--gray4);
+//   padding: 0.5rem 3rem;
+//   border-radius: 0.5rem;
+//   color: var(--gray11);
+// `;
 
 const imageStyle = css`
   width: 3rem;
 `;
 
 const fontStyle = css`
-  font-size: 0.8rem;
+  font-size: 11px;
+  font-weight: 400;
   color: var(--gray9);
 `;
 
