2e870ea76597880405a4116006cc7e888a2bdd2e
devKyu <barley12@naver.com>
2025-10-27 02:06:37 +0900

    fix: og 관련 링크 수정

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -17,13 +17,16 @@ const Footer = ({shareId}:{shareId:string}) => {
 
   const handleKakaoShareClick = async () => {
     const Kakao = await loadKakaoSdk();
+    const title = `${nameList.maleName} ❤ ${nameList.femaleName} 저희 결혼합니다!💍`;
+    const description =  `${date.year}년 ${date.month}월 ${date.day}일 ${date.hour}시 ${date.min}분`;
+    const imageUrl = `${SHARE_URL}/api/og?title=${encodeURIComponent(title)}&subtitle=${encodeURIComponent(description)}&image=${encodeURIComponent(picture?.url)}`;
 
     Kakao.Share.sendDefault({
       objectType: 'feed',
       content: {
-        title: `${nameList.maleName} ❤ ${nameList.femaleName} 저희 결혼합니다!💍`,
-        description: `${date.year}년 ${date.month}월 ${date.day}일 ${date.hour}시 ${date.min}분`,
-        imageUrl: `${picture?.url ?? ''}`,
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
         link: {
           mobileWebUrl: `${SHARE_URL}/${shareId}`,
           webUrl:`${SHARE_URL}/${shareId}`,
