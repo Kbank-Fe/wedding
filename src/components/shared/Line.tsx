f4a1d7c8189b9247af536f22f6201ba99b173753
dayeon <aakk9350@naver.com>
2025-10-27 17:17:55 +0900

    style: line 스타일 수정

--- src/components/shared/Line.tsx
+++ src/components/shared/Line.tsx
@@ -10,10 +10,10 @@ const Line = ({ marginTop = 25, marginBottom = 25 }: LineProps) => {
 };
 
 const lineStyle = (mt: number, mb: number) => css`
-  width: 70%;
+  width: 96%;
   margin: ${mt}px auto ${mb}px auto;
   border: none;
-  border-bottom: 1px solid var(--gray10);
+  border-bottom: 1px solid var(--gray6);
 `;
 
 export default Line;
