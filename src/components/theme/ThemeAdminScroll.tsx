92aa44817c6e549e47a9f38eb35d45fb18fa2c39
pjhyng0125 <pjhyng0125@naver.com>
2025-11-29 14:09:40 +0900

    feature: div 영역 클릭 이벤트 바인딩 (input radio 클릭한 것처럼)

--- src/components/theme/ThemeAdminScroll.tsx
+++ src/components/theme/ThemeAdminScroll.tsx
@@ -1,21 +1,24 @@
 import { css } from '@emotion/react';
 import React, { useEffect, useRef } from 'react';
 
-interface ThemeAdminScrollProps {
-  items: {
-    id: number;
-    image: string;
-    value: string;
-    checked: boolean;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  }[];
-}
+type ThemeAdminScrollProps = {
+  items: ThemeAdminScrollItemProps[];
+};
+
+type ThemeAdminScrollItemProps = {
+  id: number;
+  image: string;
+  value: string;
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
   const scrollerRef = useRef<HTMLDivElement | null>(null);
   const isDownRef = useRef(false);
   const startXRef = useRef(0);
   const scrollLeftRef = useRef(0);
+  const hasDraggedRef = useRef(false); // 드래그발생여부
 
   useEffect(() => {
     // 이미지 드래그 시 브라우저 기본 드래그 이벤트 방지
@@ -32,6 +35,7 @@ const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
     const el = scrollerRef.current;
     if (!el) return;
     isDownRef.current = true;
+    hasDraggedRef.current = false; // 마우스 다운 시 드래그 리셋
     el.classList.add('dragging'); // CSS cursor 변경용
     startXRef.current = e.pageX - el.offsetLeft;
     scrollLeftRef.current = el.scrollLeft;
@@ -58,6 +62,13 @@ const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
   const onMouseMove: React.MouseEventHandler = (e) => {
     const el = scrollerRef.current;
     if (!el || !isDownRef.current) return;
+
+    // 5px 이상 움직이면 드래그로 간주
+    const dx = Math.abs(e.pageX - (startXRef.current + el.offsetLeft));
+    if (dx > 5) {
+      hasDraggedRef.current = true;
+    }
+
     e.preventDefault();
     const x = e.pageX - el.offsetLeft;
     const walk = (x - startXRef.current) * 1; // 스크롤 민감도 (1 = 1:1)
@@ -69,6 +80,7 @@ const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
     const el = scrollerRef.current;
     if (!el) return;
     isDownRef.current = true;
+    hasDraggedRef.current = false; // 터치 다운 시 드래그 리셋
     startXRef.current = e.touches[0].pageX - el.offsetLeft;
     scrollLeftRef.current = el.scrollLeft;
   };
@@ -78,11 +90,38 @@ const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
     if (!el || !isDownRef.current) return;
     const x = e.touches[0].pageX - el.offsetLeft;
     const walk = (x - startXRef.current) * 1;
+
+    const dx = Math.abs(x - startXRef.current);
+    if (dx > 5) {
+      // 5px 이상 움직이면 드래그로 간주
+      hasDraggedRef.current = true;
+    }
+
     el.scrollLeft = scrollLeftRef.current - walk;
   };
 
   const onTouchEnd = () => {
     isDownRef.current = false;
+  };
+
+  const handleClickImage = (item: ThemeAdminScrollItemProps) => () => {
+    // 1. 드래그가 끝난 직후 발생한 클릭 무시
+    if (hasDraggedRef.current) {
+      return;
+    }
+
+    // 2. 이미 선택된 상태 변경 무시
+    if (item.checked) return;
+
+    // 2. 가짜 이벤트 객체를 생성하여 onChange 함수 호출
+    item.onChange({
+      target: {
+        checked: true,
+        value: item.value,
+        type: 'radio',
+        name: 'themeRadioSlide',
+      } as HTMLInputElement,
+    } as React.ChangeEvent<HTMLInputElement>);
   };
 
   return (
@@ -99,7 +138,21 @@ const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
       onTouchStart={onTouchStart}
     >
       {items.map((item) => (
-        <div key={item.id} css={itemStyle}>
+        <div
+          key={item.id}
+          css={itemStyle}
+          role="button"
+          tabIndex={0}
+          onClick={handleClickImage(item)}
+          onKeyDown={(e) => {
+            // 키보드 이벤트 핸들러 (필수)
+            // Enter 키나 Space 키를 눌렀을 때도 클릭 이벤트와 동일하게 작동하도록 처리
+            if (e.key === 'Enter' || e.key === ' ') {
+              e.preventDefault(); // 스페이스바의 기본 동작(스크롤 등) 방지
+              handleClickImage(item); // 클릭과 동일한 동작 수행
+            }
+          }}
+        >
           <img alt="" css={imgStyle(item.checked)} src={item.image} />
           <input
             checked={item.checked}
@@ -108,6 +161,7 @@ const ThemeAdminScroll = ({ items }: ThemeAdminScrollProps) => {
             type="radio"
             value={item.value}
             onChange={item.onChange}
+            onClick={(e) => e.stopPropagation()}
           />
         </div>
       ))}
@@ -173,9 +227,9 @@ const radioButtonStyle = css`
   width: 18px;
   height: 18px;
   appearance: none;
-  border: 1px solid var(--gray9);
   border-radius: 50%;
-  background: var(--gray8);
+  border: 1px solid var(--gray4);
+  background: var(--gray4);
   backdrop-filter: blur(4px);
   cursor: pointer;
 
@@ -184,14 +238,14 @@ const radioButtonStyle = css`
     position: relative; /* relative로 둬서 부모 중앙 정렬된 flex에 맞춤 */
     display: inline-block;
     font-size: 14px;
-    color: var(--gray12);
+    color: var(--gray1);
     opacity: 0;
     transition: opacity 0.15s ease;
   }
 
   &:checked {
-    background: var(--gray4);
-    border-color: var(--gray4);
+    background: var(--gray10);
+    border-color: var(--gray10);
   }
 
   &:checked::after {
