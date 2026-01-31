b85b91cdee5fa2ac3fa10ab0801918f609466c7e
JinHyeong Park <pjhyng0125@naver.com>
2026-01-31 22:39:20 +0900

    refactor: 불필요 패키지 react-hook-form 제거

--- src/components/theme/ThemeTextFields.tsx
+++ src/components/theme/ThemeTextFields.tsx
@@ -1,5 +1,4 @@
 import { useCallback } from 'react';
-import { Controller, useForm } from 'react-hook-form';
 
 import BaseEnglishTextInput from '@/components/shared/BaseEnglishTextInput';
 import Field from '@/components/shared/Field';
@@ -14,15 +13,9 @@ const ThemeTextFields = ({ localThemeItem }: Props) => {
   const setField = useWeddingStore((state) => state.setField);
   const theme = useWeddingStore((state) => state.values.theme) || {};
 
-  // react-hook-form 초기화: Zustand의 현재 값을 기본값으로 설정
-  const { control } = useForm<Record<TextAllowedKeys, string>>({
-    values: theme as Record<TextAllowedKeys, string>, // Zustand 상태와 폼 값을 동기화
-  });
-
-  // Zustand에 값 저장
   const handleChangeField = useCallback(
-    (key: TextAllowedKeys, value: string) => {
-      setField('theme', key, value);
+    (key: TextAllowedKeys) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setField('theme', key, e.target.value);
     },
     [setField],
   );
@@ -44,24 +37,11 @@ const ThemeTextFields = ({ localThemeItem }: Props) => {
             label={option.label}
             mode="single"
           >
-            <Controller
-              control={control}
-              name={key}
-              render={({ field: { onChange, value, ref } }) => (
-                <BaseEnglishTextInput
-                  ref={ref} // react-hook-form의 ref 연결
-                  maxLength={option.maxLength}
-                  placeholder={`${option.label} 입력해주세요`}
-                  value={value || ''}
-                  onChange={(e) => {
-                    // RHF 상태 업데이트
-                    onChange(e);
-
-                    // Zustand 전역 상태 업데이트
-                    handleChangeField(key, e.target.value);
-                  }}
-                />
-              )}
+            <BaseEnglishTextInput
+              maxLength={option.maxLength}
+              placeholder={`${option.label} 입력해주세요`}
+              value={theme[key] || ''}
+              onChange={handleChangeField(key)}
             />
           </Field>
         );
